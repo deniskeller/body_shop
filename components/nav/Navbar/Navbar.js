8b91abf8d901395d513ea1abef0df6a6d663ae35
deniskeller <dentean@mail.ru>
2021-08-24 23:24:34 +0400

    ref

--- components/nav/Navbar/Navbar.js
+++ components/nav/Navbar/Navbar.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import { BaseButton, NavbarLink } from '../..';
+import {
+  BaseButton,
+  BasePopup,
+  BaseSubtitle,
+  BaseTitle,
+  Layout,
+  NavbarLink,
+} from '../..';
 import Burger from '../../../assets/img/Burger';
 import LogoText from '../../../assets/img/LogoText';
 import Link from 'next/link';
@@ -14,73 +21,93 @@ const Navbar = () => {
   ];
   const [burgerActive, setBurgerActive] = React.useState(false);
 
+  const [modal, setModal] = React.useState(false);
+
   return (
-    <div className={styled.Navbar}>
-      {!burgerActive ? (
-        <div className={styled.NavbarDefault}>
-          <div className={styled.Navbar__logo}>
-            <div className={styled.Navbar__logoText}>
-              <LogoText />
-            </div>
-            <div
-              className={styled.Navbar__burger}
-              onClick={() => setBurgerActive(!burgerActive)}
-            >
-              <Burger open={burgerActive} />
+    <Layout>
+      <BasePopup visible={modal} setVisible={setModal}>
+        <BaseTitle style={{ marginBottom: 10 }}>выйти?</BaseTitle>
+
+        <BaseSubtitle style={{ marginBottom: 24 }}>
+          Ты сможешь вернуться <br /> и продолжить игру <br /> в любой момент
+        </BaseSubtitle>
+
+        <BaseButton
+          style={{ marginBottom: 16 }}
+          method={() => router.push('/')}
+        >
+          Да
+        </BaseButton>
+        <BaseButton type='clear'>Не выходить</BaseButton>
+      </BasePopup>
+
+      <div className={styled.Navbar}>
+        {!burgerActive ? (
+          <div className={styled.NavbarDefault}>
+            <div className={styled.Navbar__logo}>
+              <div className={styled.Navbar__logoText}>
+                <LogoText />
+              </div>
+              <div
+                className={styled.Navbar__burger}
+                onClick={() => setBurgerActive(!burgerActive)}
+              >
+                <Burger open={burgerActive} />
+              </div>
             </div>
           </div>
-        </div>
-      ) : (
-        <div className={styled.NavbarActive}>
-          <div className={styled.NavbarActive__logo}>
-            <div className={styled.NavbarActive__logoText}>
-              <LogoText />
+        ) : (
+          <div className={styled.NavbarActive}>
+            <div className={styled.NavbarActive__logo}>
+              <div className={styled.NavbarActive__logoText}>
+                <LogoText />
+              </div>
+              <div
+                className={styled.NavbarActive__burger}
+                onClick={() => setBurgerActive(!burgerActive)}
+              >
+                <Burger open={burgerActive} />
+              </div>
             </div>
-            <div
-              className={styled.NavbarActive__burger}
-              onClick={() => setBurgerActive(!burgerActive)}
-            >
-              <Burger open={burgerActive} />
+
+            <div className={styled.NavbarActive__userinfo}>
+              <div className={styled.NavbarActive__usermail}>
+                konstantinopol@gmail.com
+              </div>
+              <div className={styled.NavbarActive__userplace}>103 место</div>
+              <div className={styled.NavbarActive__userrecorde}>
+                Рекорд за неделю: 1300
+              </div>
             </div>
-          </div>
 
-          <div className={styled.NavbarActive__userinfo}>
-            <div className={styled.NavbarActive__usermail}>
-              konstantinopol@gmail.com
+            <div className={styled.NavbarActive__nav}>
+              {links.map((link) => {
+                return (
+                  <span key={link.id}>
+                    <NavbarLink href={link.href} title={link.title} />
+                  </span>
+                );
+              })}
             </div>
-            <div className={styled.NavbarActive__userplace}>103 место</div>
-            <div className={styled.NavbarActive__userrecorde}>
-              Рекорд за неделю: 1300
-            </div>
-          </div>
 
-          <div className={styled.NavbarActive__nav}>
-            {links.map((link) => {
-              return (
-                <span key={link.id}>
-                  <NavbarLink href={link.href} title={link.title} />
-                </span>
-              );
-            })}
-          </div>
+            <BaseButton type='clear' style={{ marginBottom: 40 }}>
+              Выйти
+            </BaseButton>
 
-          <BaseButton type='clear' style={{ marginBottom: 40 }}>
-            Выйти
-          </BaseButton>
+            <Link href='/politics'>
+              <p className={styled.NavbarActive__text}>
+                Политика обработки <br /> персональных данных
+              </p>
+            </Link>
 
-          <Link href='/politics'>
             <p className={styled.NavbarActive__text}>
-              Политика обработки <br /> персональных данных
+              © 2021. The Body Shop.
+              <br /> Все права защищены.
             </p>
-          </Link>
-
-          <p className={styled.NavbarActive__text}>
-            © 2021. The Body Shop.
-            <br /> Все права защищены.
-          </p>
-        </div>
-      )}
-    </div>
+          </div>
+        )}
+      </div>
+    </Layout>
   );
 };
 
